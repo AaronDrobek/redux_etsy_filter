600338a3edf4f1260c2c11ce0c832443e1181467
Aaron Drobek <adrobek09@gmail.com>
2017-09-27 17:49:41 -0400

    for sean

---
+++ src/containers/ProductList.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux'
+
+import Product from "../components/product";
+
+class ProductList extends Component {
+
+    render() {
+
+        const {products} = this.props;
+        let babes = products.map((babe,index) => {
+          return (
+            <Product product={babe} key={index} />
+          )
+        })
+
+
+
+        // Create a dynamically populated list of `<Product />` components
+        // Each `<Product />` component should have a single object from the `products` state property (array)
+        // applied to the component as a `product` property
+        return (
+            <ul className="ProductList">
+              {babes}
+            </ul>
+        );
+    }
+}
+
+// Using the `mapStateToProps` function, filter the array stored in the
+// state `products` property based on 3 criterea:
+// - `underTwenty`
+// - `overTwenty`
+// - `all` or the default
+const mapStateToProps = function(state) {
+    let products;
+    // complete the `if else` statement including conditions and `products` value
+    if (state.filter === 'underTwenty') {
+      products = state.products.filter(product => product.price <= 20)
+    } else if (state.filter === 'overTwenty') {
+      products = state.products.filter(product => product.price >= 20)
+    } else {
+      products = state.products
+    }
+
+    return {products: products}
+}
+
+export default connect(mapStateToProps)(ProductList);
